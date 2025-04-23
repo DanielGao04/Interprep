4647489796ac3787e1e06f8087ade8ac0c3fa173
DanielGao04 <danielgao341@gmail.com>
2025-04-23 17:44:52 -0400

    new

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { ReactNode } from 'react'
 
-const layout = () => {
+const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>layout</div>
+    <div>{children}</div>
   )
 }
 
-export default layout
+export default AuthLayout
