262a55e69cd1c6d895ccda59da2ea930ae6913ec
DanielGao04 <danielgao341@gmail.com>
2025-04-28 15:29:03 -0400

    75876

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -9,16 +9,16 @@ import {
   Form,
   FormControl,
   FormDescription,
-  FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import FormField from "@/components/FormField"
 import { Input } from "@/components/ui/input"
 import Image from "next/image";
 import Link from 'next/link';
 import { toast } from "sonner";
-
+import { useRouter } from "next/navigation"
 
 
 const authFormSchema = (type: FormType) => {
@@ -30,6 +30,7 @@ const authFormSchema = (type: FormType) => {
 }
 
 const AuthForm = ({ type }: { type: FormType}) => {
+  const router = useRouter()
   const formSchema = authFormSchema(type);
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -44,9 +45,11 @@ const AuthForm = ({ type }: { type: FormType}) => {
   function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if(type === "sign-up"){
-        console.log('SIGN UP', values)
+        toast.success("Account created successfully! Please sign in.")
+        router.push('/sign-in')
       } else {
-        console.log('SIGN IN', values)
+        toast.success("Signed in successfully!")
+        router.push('/')
       }
     } catch (error) {
       console.error(error)
@@ -72,9 +75,11 @@ const AuthForm = ({ type }: { type: FormType}) => {
         
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
-                    {!isSignIn && <p>Name</p>}
-                    <p>Email</p>
-                    <p>Password</p>
+                    {!isSignIn && (
+                      <FormField control={form.control} name="name" label="Name" placeholder="Your Name"/>
+                    )}
+                    <FormField control={form.control} name="email" label="Email" placeholder="Your email address" type="email"/>
+                    <FormField control={form.control} name="password" label="Password" placeholder="Enter your password" type="password"/>
 
 
                     <Button className="btn" type="submit">{isSignIn ? 'Sign in': 'Create an Account'} </Button>
