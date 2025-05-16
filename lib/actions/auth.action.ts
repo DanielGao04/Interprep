4d2389c753daf0ba7ef82ed0ef668a07cd433710
Daniel Gao <danielgao341@gmail.com>
2025-05-15 20:40:21 -0400

    Implement signUp function with user creation error handling

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,3 +1,26 @@
+'use server';
+
+import { db } from "@/firebase/admin";
+
 export async function signUp(params: SignUpParams) {
-    
+    const { uid, name, email } = params;
+
+    try{
+        const userRecord = await db .collection('users').doc(uid).get()
+
+    } catch (e: any) {
+        console.error('Error creating a user', e)
+        if(e.code === 'auth/email-already-exists') {
+            return {
+                success: false ,
+                message: 'Email already in use'
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Error creating an account'
+        }
+
+    }
 }
