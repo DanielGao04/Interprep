eb94a0d9af287d1490b4684513ac39f53f6c7c10
Daniel Gao <danielgao341@gmail.com>
2025-05-15 21:53:40 -0400

    Implement user existence check in signUp function

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -8,6 +8,18 @@ export async function signUp(params: SignUpParams) {
     try{
         const userRecord = await db .collection('users').doc(uid).get()
 
+        if(userRecord.exists) {
+            return {
+                success: false,
+                message: 'User already exists. Please sign in.'
+            }
+        }
+        
+        await db.collection('users').doc(uid).set({
+            name, email,
+        })
+
+
     } catch (e: any) {
         console.error('Error creating a user', e)
         if(e.code === 'auth/email-already-exists') {
