4b27c8a0f8dc754d08938193bf3229348b389848
Daniel Gao <danielgao341@gmail.com>
2025-06-01 14:20:02 -0400

    Replace user avatar image format from PNG to JPG for improved compatibility and remove unused PNG file.

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -151,7 +151,7 @@ const Agent = ({ userName, userId, type, questions, interviewId, feedbackId}: Ag
         </div>
         <div className="card-border">
             <div className="card-content">
-                <Image src="/user-avatar.png" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px] " />
+                <Image src="/user-avatar.jpg" alt="user avatar" width={540} height={540} className="rounded-full object-cover size-[120px] " />
                 <h3>{userName}</h3>
             </div>
         </div>
